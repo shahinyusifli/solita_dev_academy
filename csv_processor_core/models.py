9898a7211ad74df1f7feaa5b2d718d92449a3b3c
shahinyusifli_ <sahinyusifli98@gmail.com>
2022-01-02 22:30:47 +0400

    I create Farms table in model.py which holds data parsed from CSV file.
    After created table with ORM I migrate all changes for creating table when I execute 'python manage.py makemigrations' command in cmd it creates tables in databse for other additional staff such as admin, user registration.

--- csv_processor_core/models.py
+++ csv_processor_core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Farms(models.Model):
+    location = models.CharField(max_length=150)
+    date_time = models.DateTimeField()
+    sensor_type = models.CharField(max_length=50)
+    values = models.IntegerField()
 
+
+    def __str__(self):
+        return self.location
