a7e2a8ec23c3783be4870b3b0ea99d62c3e2c831
shahinyusifli_ <sahinyusifli98@gmail.com>
2022-01-10 05:36:35 +0400

    I create sample data with the setUpTestData function for using them in other functions to test the Farm model. Also, I add functions to FarmModelTest class for checking the existing and livability of labels in the Farm model.

--- csv_processor_core/tests/test_model.py
+++ csv_processor_core/tests/test_model.py
@@ -4,4 +4,40 @@ from csv_processor_core.models import Farms
 
 class FarmModelTest(TestCase):
 
-    pass
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Farms.objects.create(
+            location="Test farm", 
+            date_time="2019-01-01T06:09:47.373Z",
+            sensor_type = "pH",
+            values = 7,
+            )
+
+    # Check existing labels
+    # Check location label
+    def test_location_label(self):
+        farm = Farms.objects.get(id=1)
+        field_label = farm._meta.get_field("location").verbose_name
+        self.assertEqual(field_label, "location")
+
+    # Check date_time label
+    def test_date_time_label(self):
+        farm = Farms.objects.get(id=1)
+        field_label = farm._meta.get_field("date_time").verbose_name
+        self.assertEqual(field_label, "date time")
+
+    # Check sensor_type label
+    def test_sensor_type_label(self):
+        farm = Farms.objects.get(id=1)
+        field_label = farm._meta.get_field("sensor_type").verbose_name
+        self.assertEqual(field_label, "sensor type")
+
+    # Check values label
+    def test_values_label(self):
+        farm = Farms.objects.get(id=1)
+        field_label = farm._meta.get_field('values').verbose_name
+        self.assertEqual(field_label, 'values')
+
+    
