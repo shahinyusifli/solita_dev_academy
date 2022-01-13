3aa596c6ca6d2423ebfef0a99fe8e48a918e10a4
shahinyusifli_ <sahinyusifli98@gmail.com>
2022-01-13 20:02:29 +0400

    I create a table functionality for Farms data that admins can see Farms data in the table.
    FarmsAdmin class is created in admin.py

--- csv_processor_core/admin.py
+++ csv_processor_core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Farms
 
+
+class FarmsAdmin(admin.ModelAdmin):
+    list_display = ('location', 'date_time', 'sensor_type', 'values')
+
 # Register your models here.
-admin.site.register(Farms)
+admin.site.register(Farms, FarmsAdmin)
